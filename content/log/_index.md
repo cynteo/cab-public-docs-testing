ba31c2d48ab7052058c9bdf23ef8c3e8f82c2c7c
mejdm5 <david.mejak@cynteocloud.com>
2025-10-30 11:17:41 -0400

    new theme, adding test pages

---
+++ content/log/_index.md
@@ -0,0 +1,12 @@
++++
+title = "1. Quick Links"
+type = "default"
+weight = 1
++++
+
+## 🚀 Quick Links
+
+- **[Get Started](https://cab-docs.cynteocloud.com/getting-started/quickstart)** - Deployment guide
+- **[Features](https://cab-docs.cynteocloud.com#-key-features)** - What Alert Bridge can do
+- **[Troubleshooting](https://cab-docs.cynteocloud.com/troubleshooting/common-issues)** - Common issues
+- **[Support](mailto:support@cynteocloud.com)** - Get help
---
+++ public/searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Cynteo Alert Bridge Docs",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "Log",
+    "uri": "/log/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Cynteo Alert Bridge Docs",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Cynteo Alert Bridge Docs",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "Cynteo Alert Bridge Docs",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Cynteo Alert Bridge Docs",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
